1c1fd8210a91344092fbaa3eda706db5579673e1
chenyi <228112142@qq.com>
2018-02-27 17:05:54 +0800

    开启xss防护

--- cy-common/src/main/java/xin/cymall/xss/XssHttpServletRequestWrapper.java
+++ cy-common/src/main/java/xin/cymall/xss/XssHttpServletRequestWrapper.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * XSS过滤处理
@@ -22,7 +24,7 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
     //没被包装过的HttpServletRequest（特殊场景，需要自己过滤）
     HttpServletRequest orgRequest;
     //html过滤
-//    private final static HTMLFilter htmlFilter = new HTMLFilter();
+   private final static HTMLFilter htmlFilter = new HTMLFilter();
 
     public XssHttpServletRequestWrapper(HttpServletRequest request) {
         super(request);
@@ -43,7 +45,7 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
         }
 
         //xss过滤
-       // json = xssEncode(json);
+        json = xssEncode(json);
         final ByteArrayInputStream bis = new ByteArrayInputStream(json.getBytes("utf-8"));
         return new ServletInputStream() {
 
@@ -67,54 +69,54 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
         };
     }
 
-//    @Override
-//    public String getParameter(String name) {
-//        String value = super.getParameter(xssEncode(name));
-//        if (StringUtils.isNotBlank(value)) {
-//            value = xssEncode(value);
-//        }
-//        return value;
-//    }
+    @Override
+    public String getParameter(String name) {
+        String value = super.getParameter(xssEncode(name));
+        if (StringUtils.isNotBlank(value)) {
+            value = xssEncode(value);
+        }
+        return value;
+    }
 
-//    @Override
-//    public String[] getParameterValues(String name) {
-//        String[] parameters = super.getParameterValues(name);
-//        if (parameters == null || parameters.length == 0) {
-//            return null;
-//        }
-//
-//        for (int i = 0; i < parameters.length; i++) {
-//            parameters[i] = xssEncode(parameters[i]);
-//        }
-//        return parameters;
-//    }
+    @Override
+    public String[] getParameterValues(String name) {
+        String[] parameters = super.getParameterValues(name);
+        if (parameters == null || parameters.length == 0) {
+            return null;
+        }
 
-//    @Override
-//    public Map<String,String[]> getParameterMap() {
-//        Map<String,String[]> map = new LinkedHashMap<>();
-//        Map<String,String[]> parameters = super.getParameterMap();
-//        for (String key : parameters.keySet()) {
-//            String[] values = parameters.get(key);
-//            for (int i = 0; i < values.length; i++) {
-//                values[i] = xssEncode(values[i]);
-//            }
-//            map.put(key, values);
-//        }
-//        return map;
-//    }
+        for (int i = 0; i < parameters.length; i++) {
+            parameters[i] = xssEncode(parameters[i]);
+        }
+        return parameters;
+    }
 
-//    @Override
-//    public String getHeader(String name) {
-//        String value = super.getHeader(xssEncode(name));
-//        if (StringUtils.isNotBlank(value)) {
-//            value = xssEncode(value);
-//        }
-//        return value;
-//    }
+    @Override
+    public Map<String,String[]> getParameterMap() {
+        Map<String,String[]> map = new LinkedHashMap<>();
+        Map<String,String[]> parameters = super.getParameterMap();
+        for (String key : parameters.keySet()) {
+            String[] values = parameters.get(key);
+            for (int i = 0; i < values.length; i++) {
+                values[i] = xssEncode(values[i]);
+            }
+            map.put(key, values);
+        }
+        return map;
+    }
 
-//    private String xssEncode(String input) {
-//        return htmlFilter.filter(input);
-//    }
+    @Override
+    public String getHeader(String name) {
+        String value = super.getHeader(xssEncode(name));
+        if (StringUtils.isNotBlank(value)) {
+            value = xssEncode(value);
+        }
+        return value;
+    }
+
+    private String xssEncode(String input) {
+        return htmlFilter.filter(input);
+    }
 
     /**
      * 获取最原始的request
